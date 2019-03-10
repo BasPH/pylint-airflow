6f4739d2a79de874ea610fcf45e635e64f832b10
Bas Harenslak <basharenslak@gmail.com>
2019-03-10 20:08:27 +0100

    Added unused-xcom

--- src/pylint_airflow/checkers/xcom.py
+++ src/pylint_airflow/checkers/xcom.py
@@ -1,7 +1,8 @@
 """Checks on Airflow XComs."""
-
+import astroid
 from pylint import checkers
 from pylint import interfaces
+from pylint.checkers import utils
 
 from pylint_airflow.__pkginfo__ import BASE_ID
 
@@ -13,9 +14,69 @@ class XComChecker(checkers.BaseChecker):
 
     msgs = {
         f"R{BASE_ID}00": (
-            "Return value from {} is stored as XCom but not used anywhere",
+            "Return value from %s is stored as XCom but not used anywhere",
             "unused-xcom",
             "Return values from a python_callable function or execute() method are "
             "automatically pushed as XCom.",
         )
     }
+
+    @utils.check_messages("unused-xcom")
+    def visit_module(self, node: astroid.Module):
+        """
+        Check for unused XComs.
+        XComs can be set (pushed) implicitly via return of a python_callable or
+        execute() of an operator. And explicitly by calling xcom_push().
+
+        Currently this only checks unused XComs from return value of a python_callable.
+        """
+        assign_nodes = [n for n in node.body if isinstance(n, astroid.Assign)]
+        call_nodes = {
+            n.targets[0].name: n.value for n in assign_nodes if isinstance(n.value, astroid.Call)
+        }
+
+        # Store nodes containing python_callable arg as:
+        # {task_id: (call node, python_callable func name)}
+        python_callable_nodes = dict()
+        for call_varname, call_node in call_nodes.items():
+            if call_node.keywords:
+                task_id = ""
+                python_callable = ""
+                for keyword in call_node.keywords:
+                    if keyword.arg == "python_callable":
+                        python_callable = keyword.value.name
+                        continue
+                    elif keyword.arg == "task_id":
+                        task_id = keyword.value.value
+
+                if python_callable:
+                    python_callable_nodes[task_id] = (call_node, python_callable)
+
+        # Now fetch the functions mentioned by python_callable args
+        xcoms_pushed = dict()
+        xcoms_pulled_taskids = set()
+        for (task_id, (python_callable, callable_func_name)) in python_callable_nodes.items():
+            callable_func: astroid.FunctionDef = node.getattr(callable_func_name)[0]
+
+            # Check if the function returns any values
+            returns_value = any([isinstance(n, astroid.Return) for n in callable_func.body])
+            if returns_value:
+                xcoms_pushed[task_id] = (python_callable, callable_func_name)
+
+            # Check if the function pulls any XComs
+            callable_func_calls = callable_func.nodes_of_class(astroid.Call)
+            for callable_func_call in callable_func_calls:
+                if (
+                    isinstance(callable_func_call.func, astroid.Attribute)
+                    and callable_func_call.func.attrname == "xcom_pull"
+                ):
+                    for keyword in callable_func_call.keywords:
+                        if keyword.arg == "task_ids":
+                            xcoms_pulled_taskids.add(keyword.value.value)
+
+        remainder = xcoms_pushed.keys() - xcoms_pulled_taskids
+        if remainder:
+            # There's a remainder in xcoms_pushed_taskids which should've been xcom_pulled.
+            for remainder_task_id in remainder:
+                python_callable, callable_func_name = xcoms_pushed[remainder_task_id]
+                self.add_message("unused-xcom", node=python_callable, args=callable_func_name)
---
+++ tests/pylint_airflow/checkers/test_xcom.py
@@ -0,0 +1,50 @@
+import astroid
+from pylint.testutils import CheckerTestCase, Message
+
+import pylint_airflow
+
+
+class TestXComChecker(CheckerTestCase):
+    CHECKER_CLASS = pylint_airflow.checkers.xcom.XComChecker
+
+    def test_used_xcom(self):
+        testcase = """
+        from airflow.operators.python_operator import PythonOperator
+        
+        def _pushtask():
+            print("do stuff")
+            return "foobar"
+        
+        pushtask = PythonOperator(task_id="pushtask", python_callable=_pushtask)
+            
+        def _pulltask(task_instance, **_):
+            print(task_instance.xcom_pull(task_ids="pushtask"))
+            
+        pulltask = PythonOperator(task_id="pulltask", python_callable=_pulltask, provide_context=True)
+        """
+        ast = astroid.parse(testcase)
+        with self.assertNoMessages():
+            self.checker.visit_module(ast)
+
+    def test_unused_xcom(self):
+        testcase = """
+        from airflow.operators.python_operator import PythonOperator
+
+        def _pushtask():
+            print("do stuff")
+            return "foobar"
+
+        pushtask = PythonOperator(task_id="pushtask", python_callable=_pushtask)
+
+        def _pulltask(task_instance, **_):
+            print("foobar")
+
+        pulltask = PythonOperator(task_id="pulltask", python_callable=_pulltask, provide_context=True)
+        """
+        ast = astroid.parse(testcase)
+        expected_msg_node = ast.body[2].value
+        expected_args = "_pushtask"
+        with self.assertAddsMessages(
+            Message(msg_id="unused-xcom", node=expected_msg_node, args=expected_args)
+        ):
+            self.checker.visit_module(ast)
