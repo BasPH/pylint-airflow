adcbe85cfc2d0dd8d09fa6429a273d40fe0ec6fb
Bas Harenslak <basharenslak@gmail.com>
2019-03-17 12:04:36 +0100

    Place all requirements in setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,26 @@
 
 from setuptools import setup, find_packages
 
+requirements = ["pylint"]
+dev_requirements = ["apache-airflow~=1.10.2", "black>=18.9b0", "pytest~=4.3"]
+
 setup(
     name="pylint-airflow",
     url="https://github.com/BasPH/pylint-airflow",
     description="A Pylint plugin to lint Apache Airflow code.",
-    version="0.0.1",
+    version="0.1.0-alpha",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=["pylint"],
+    install_requires=requirements,
+    extras_require={"dev": dev_requirements},
     keywords=["pylint", "airflow", "plugin"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
